67d4d864f9b7350ab991302136b6e21c3865b711
zhangchi <zhangchi194cm@gmail.com>
2020-12-13 14:44:10 +0800

    Try to deploy.

--- js/download.js
+++ js/download.js
@@ -1,12 +1,5 @@
 // 点击示例文件下载
-// $("#demo1").click(function(){
-//     var $eleForm = $("<form method='get'></form>");
-//     $eleForm.attr("action",host + '/downloads/appendix_v0.3.exe');
-//     $(document.body).append($eleForm);
-//     //提交表单，实现下载
-//     $eleForm.submit();
-// })
-function myFunction() {
-    document.getElementById("demo").innerHTML = "段落被更改。";
+function download() {
+    window.location.href='../downloads/appendix_v0.3.exe';
 }
 
